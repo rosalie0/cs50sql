694bd1c6d9ca2edf9745335f6e94ab5192ed0407
Rosalie Newman <rosalienewman0@gmail.com>
2023-04-24 12:48:39 -0400

    songs7 passing

---
+++ cs50x_week7_sql/labs/commands.md
@@ -0,0 +1,6 @@
+# command to check if passing:
+
+```bash
+check50 --local cs50/labs/2023/x/songs
+check50 cs50/labs/2023/x/songs
+```
--- cs50x_week7_sql/labs/songs/7.sql
+++ cs50x_week7_sql/labs/songs/7.sql
@@ -1,3 +1,13 @@
 --In 7.sql, write a SQL query that returns the average energy of songs that are by Drake.
 --Your query should output a table with a single column and a single row containing the average energy.
 --You should not make any assumptions about what Drake's artist_id is.
+
+-- need a SELECT to get drake's id (this is the 2nd query)
+-- psuedo: select avg energy in (2nd query)
+
+SELECT AVG("energy") FROM "songs"
+  WHERE "artist_id" 
+    IN (
+      -- get drake's id from artist table
+      SELECT "id" FROM "artists" WHERE "name" = 'Drake'
+    );
